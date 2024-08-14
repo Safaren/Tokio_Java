d26bb6554c4fe8813eeb783602256c7c77de3de7
safaren <sergioaparicio@gmail.com>
2024-08-14 12:48:04 +0200

    i

--- Calculadora/src/main/java/main/main.java
+++ Calculadora/src/main/java/main/main.java
@@ -1,10 +1,18 @@
 package main;
 
+import javax.swing.JFrame;
+
 public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		J
+		JFrame jframe = new JFrame("Calculadora");
+		jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		jframe.setSize(400, 400);
+		jframe.setTitle("Calculadora");
+		jframe.setLocation(100,100);
+		jframe.setResizable(false);
+		
 
 	}
 
